faecb6f5a7f46e69f63d134fcb984779a749f899
akkarao <41664862+akkarao@users.noreply.github.com>
2025-05-26 19:12:36 +0530

    Update TrackingNumberApiApplication.java

--- src/main/java/com/number/tracking/TrackingNumberApiApplication.java
+++ src/main/java/com/number/tracking/TrackingNumberApiApplication.java
@@ -1,1 +1,11 @@
+package com.number.tracking;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingGeneratorNumberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrackingGeneratorNumberApplication.class, args);
+    }
+}
